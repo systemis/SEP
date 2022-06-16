3adf0a7baabf4a90f1ed1576fdde469130ddb805
systemis <cuthinh2002@icloud.com>
2022-06-16 17:27:14 +0700

    feat: initial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,10 @@ import makeStore from '@/app-redux';
 
 const store = makeStore();
 
+const AppComponent = ({ Component, pageProps }: any) => {
+  return <Component {...pageProps} />;
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
@@ -13,7 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet" />
         <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet' />
       </Head>
-      <Component {...pageProps} />
+      <AppComponent Component={Component} pageProps={pageProps} />
     </Provider>
   );
 }
